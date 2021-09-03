5ba62e8673aaf47ad2d6cf6ac00af3a57a58b0d8
navedaraj <90040464+navedaraj@users.noreply.github.com>
2021-09-03 20:02:58 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("FIS System 1234 webhook");  
+     System.out.println("FIS System 1234 webhook 123");  
     }  
 }  
